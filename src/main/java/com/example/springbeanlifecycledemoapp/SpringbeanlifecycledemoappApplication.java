253ab7820a5266252d8b1a9b67b227aad86fdffe
Rajesh Singh <mailtorajeshksingh@gmail.com>
2019-02-02 12:32:22 +0530

    Initial Project: Spring Bean Life Cycle Demo Application.

---
+++ src/main/java/com/example/springbeanlifecycledemoapp/SpringbeanlifecycledemoappApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbeanlifecycledemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeanlifecycledemoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbeanlifecycledemoappApplication.class, args);
+    }
+
+}
+
